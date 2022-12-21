25959e465f4df815b35ae614459acb132269a75d
Takashi Fujisaki <ejointjp@Takashinonotopasokon.local>
2022-12-21 23:25:58 +0900

    2022-12-21

--- _link.sh
+++ _link.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # シンボリックリンクの作成
-DOT_FILES=".gitconfig .zshrc .zprofile"
+DOT_FILES=".gitconfig .zshrc .zprofile Brewfile"
 for file in $DOT_FILES
 do
     ln -sf `pwd`/$file ~
--- setup.sh
+++ setup.sh
@@ -7,11 +7,20 @@ xcode-select --install
 # rosettaのインストール。不要であれば下記1行削除してください
 sudo softwareupdate --install-rosetta --agree-to-licensesudo softwareupdate --install-rosetta --agree-to-license
 
+### シンボリックリンクの作成
+echo "シンボリックリンクを作成します..."
+chmod 775 ~/dotfiles/_link.sh
+~/dotfiles/_link.sh
+
 #------------------------------------------
 # homebrew(arm64)
 #------------------------------------------
 echo "homebrewをインストールします..."
 which /opt/homebrew/bin/brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/wataree/.zprofile
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/wataree/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "brew doctorを実行します..."
 which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew doctor
@@ -22,7 +31,7 @@ which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew update --verbose
 echo "brew upgradeを実行します..."
 which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew upgrade --verbose
 
-echo ".Brewfileで管理しているアプリケーションをインストールします..."
+echo "Brewfileで管理しているアプリケーションをインストールします..."
 which /opt/homebrew/bin/brew >/dev/null 2>&1 && brew bundle --file ./Brewfile --verbose
 
 echo "brew cleanupを実行します..."
@@ -32,9 +41,6 @@ which brew >/dev/null 2>&1 && brew cleanup --verbose
 # echo "プログラミング言語をインストールします..."
 # ./_asdf.sh
 
-### シンボリックリンクの作成
-echo "シンボリックリンクを作成します..."
-chmod 775 ~/dotfiles/_link.sh
-~/dotfiles/_link.sh
+
 
 exec $SHELL -l
