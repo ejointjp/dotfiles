fae26fea77da427fe27e6e3b21bf7a0344a313d3
ejointjp <mail@e-joint.jp>
2024-01-27 13:26:55 +0900

    2024-01-27 13:26:54

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,17 +2,17 @@ dofile(hs.configdir .. "/dofile/autoCompressVideo.lua")
 
 local hotKey = require("modules/hotKey")
 
-hotKey.openFolder({"command", "control"}, "D", "~/Dropbox")
-hotKey.openFolder({"command", "control"}, "S", "~/Dropbox/_Screenshot")
+hotKey.openFolder({"command", "control"}, "U", "~/Dropbox")
+hotKey.openFolder({"command", "control"}, "[", "~/Dropbox/_Screenshot")
 
 hotKey.launchApp({"command", "control"}, "J", "Google Chrome")
 hotKey.launchApp({"command", "control"}, "L", "Obsidian")
-hotKey.launchApp({"command", "control"}, "N", "Notion")
+hotKey.launchApp({"command", "control"}, "K", "Notion")
 hotKey.launchApp({"command", "control"}, ";", "Visual Studio Code");
 hotKey.launchApp({"command", "control"}, "space", "Hyper");
 hotKey.launchApp({"command", "control"}, "return", "Finder")
 
-hotKey.openURL({"command", "control"}, "K", "https://chat.openai.com/?model=gpt-4")
+hotKey.openURL({"command", "control"}, "]", "https://chat.openai.com/?model=gpt-4")
 
 hotKey.openFinderDirectory({"command", "control"}, "O", "Hyper")
 
--- _link.sh
+++ _link.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # シンボリックリンクの作成
-DOT_FILES=".gitconfig .zshrc .zprofile .hyper.js Brewfile"
+DOT_FILES=".gitconfig .zshrc .zprofile .hyper.js .hammerspoon Brewfile"
 for file in $DOT_FILES
 do
     ln -sf `pwd`/$file ~
